a38c72ade9d2aa30d5a3a97b73d2bbb93c41dfe5
MrDogukan <aliseslikaya@gmail.com>
2021-12-02 21:49:56 +0300

    third commit

--- src/components/ClassComp.js
+++ src/components/ClassComp.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 export default class ClassComp extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {student:"Yusuf"};
+  }
   render() {
     return (
       <div>
         <h1>Welcome to ReactJS from CC</h1>
         <p>Age: {this.props.old}</p>
         <p>Age: {this.props.car}</p>
+        <p>Student: {this.state.student}</p>
       </div>
     );
   }
